faf9215be4f8475659a29b42512e7a779a46a96a
Oscar Mata <omata@applaudostudios.com>
2018-11-05 12:35:56 -0600

    Added information on Number methods and user input

--- introduction_to_ruby.rb
+++ introduction_to_ruby.rb
@@ -54,15 +54,7 @@ Boolean: true, false
 Nil: has not value
 
 A quotation mark can be scaped inside of a string using a backslash ("\"). Just like JS
-=end
 
-puts "She said: \"Hello there\""
-
-channel = "freeCodeCamp"
-
-puts "this tutorial can be found in " + channel + "'s YouTube channel"
-
-=begin 
 String methods:
 
 - .upcase(): turns the string all to capital letters
@@ -73,9 +65,60 @@ String methods:
 
 - .length(): shows the amount of characters in the string
 
-- .include? "": the string passed in as arguments checks if the string given is included in the evaluated string. The response is a boolean
+- .include? "": the string passed in as argument checks if the string given is included in the evaluated string. The response is a boolean
 
 - []: the bracket is used to access the character at the specified index of a string. Like in JS, the index starts at index 0; a range can be passed in, the first argument is the starting position and second one is the index where the range will stop (not including the character at that index). E.g: string[0,3]
 
-- .index(): it receives an argument, the result is the index in which the passed in argument is found in the string. If the argument is more than one letter the result is the index in where the string passed starts. If the argument passed in is not included in the string there's not going to be any result
+- .index(): it receives an argument, the result is the index in which the passed in argument is found in the string. If the argument is more than one letter the result is the index in where the string passed starts. If the argument passed in is not included in the string there's not going to be any result; this method is case sensitive
 =end
+
+puts "She said: \"Hello there\""
+
+channel = "freeCodeCamp"
+
+puts "this tutorial can be found in " + channel + "'s YouTube channel"
+
+=begin 
+Number methods:
+
+- Numbers in Ruby accept basic arithmetic operations (+, -, /, *)
+
+- To elevate a number to certain power we write the number with two asterisks and the power to elevate (2**3)
+
+- To print a number with a string, it is necessary to append the variable name to the string, but we also need to transform the number into a string with the method ".to_s"
+
+- .abs() gets the absolute value
+
+- .round() rounds to number to the nearest whole integer
+
+- .ceil(), .floor() print the number to its nearest higher and nearest lower integer respectively
+=end
+
+num = 19
+
+puts ("My favorite number is " + num.to_s)
+
+=begin 
+User Input:
+
+- "gets" waits for the user to enter some text
+
+- To submit some text with "gets" it is necessary to hit enter to send the string, but that includes a new line character. To avoid that, we can add .chomp() after gets, and that will get rid of the newline character after submitting the text
+
+- Ruby evaluates everything entered with "gets" as a String
+
+- To transform a number entered with "gets" from String to Integer we need to pass in the method ".to_i"
+=end
+
+puts "Enter your name:"
+name = gets.chomp()
+puts "Enter your age:"
+age = gets.chomp()
+puts ("Hello " + name + ", you are " + age + " years old.")
+
+puts "Enter a number:"
+num1 = gets.chomp()
+puts "Enter another number:"
+num2 = gets.chomp()
+
+puts (num1.to_i + num2.to_i)
