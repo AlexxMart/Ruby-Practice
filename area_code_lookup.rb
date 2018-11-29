769f4f5752e1d5e468d2d325b7372665dd71684e
Oscar Mata <omata@applaudostudios.com>
2018-11-28 18:45:23 -0600

    Added mini project of area code lookup

--- area_code_lookup.rb
+++ area_code_lookup.rb
@@ -13,16 +13,26 @@ dial_book = {
  
 # Get city names from the hash
 def get_city_names(somehash)
-# Write code here
+ somehash.keys
 end
  
 # Get area code based on given hash and key
 def get_area_code(somehash, key)
-# Write code here
+  somehash[key]
 end
  
 # Execution flow
 loop do
-# Write your program execution code here
+  puts "Do you want to lookup an area code based on a city name? (Y/N)"
+  answer = gets.chomp.downcase
+  break if answer != "y"
+    puts "Which city do you want to look the area code for?"
+    puts get_city_names(dial_book)
+    puts "Enter your selection:"
+    prompt = gets.chomp.downcase
+    if dial_book.include?(prompt)
+      puts "The area code for #{prompt} is #{get_area_code(dial_book, prompt)}"
+    else
+      puts "You've entered an invalid city name."
+    end
 end
- 
