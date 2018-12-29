d045f1b6380d721bc95ebb8945a749e16762aee8
AlexxMart <devalexander19@gmail.com>
2018-12-28 22:21:24 -0600

    Added the Article class to the routes, created the views for the create and new pages. Also, Added the necessary error messages to the UI

--- alpha-blog/config/routes.rb
+++ alpha-blog/config/routes.rb
@@ -3,4 +3,7 @@ Rails.application.routes.draw do
   get 'pages/home', to: 'pages#home'
   get 'pages/about', to: 'pages#about'
 
+  resources :articles
+
+  # adding a class to the routes generates a route for each method available to that class
 end
